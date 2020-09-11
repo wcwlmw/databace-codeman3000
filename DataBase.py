037c4ad57d76a6ab3f31b13a7b03dded2554f29a
Leon N <leon@escape.org>
2020-09-10 18:40:55 -0700

    Add original Database.py which Leon has been iterating on separately

---
+++ DataBase.py
@@ -0,0 +1,93 @@
+import pickle
+import time
+
+
+def First_Time():
+    choice = input('Type first thing to add to database ')
+    passcode = input('Please type what you want your passcode to be ')
+    DataBase = [choice]
+    while choice != 'DONE':
+        choice = input('type next item to add to database, if finished type DONE ')
+        if choice != 'DONE':
+            DataBase.append(choice)
+            
+
+
+    
+    pickle.dump(DataBase, open('DataBase.Dat','wb'))
+    
+    print('done')
+    time.sleep(9)
+    raise
+        
+
+
+
+
+def Normal():
+    while True:
+        DataBase = pickle.load(open('DataBase.Dat','rb'))
+        
+        print("Do you want to")
+        print("1. View Database\n2. Add to the Database\n3. Delete items")
+        print("")
+        print(" type 1 2 or 3")
+        choice = input('')
+        
+    
+        if int(choice) == 1:
+            
+            print(DataBase)
+
+        elif int(choice) == 2:
+            choice = input('Type first thing to add to database ')
+            DataBase.append(choice)
+            while choice != 'DONE':
+                choice = input('type next item to add to database, if finished type DONE ')
+                if choice != 'DONE':
+                    DataBase.append(choice)
+
+
+
+        elif int(choice) == 3:
+            print(DataBase)
+                 
+            
+            print("")
+            print("")
+            print("Which Item to delete? Enter name ")
+            choice = input("")
+            
+            
+            
+            DataBase.remove(choice)
+            
+                
+
+
+
+
+
+
+            
+        pickle.dump(DataBase, open('DataBase.Dat','wb'))    
+
+
+
+
+
+
+            
+
+code = input("Hi (imput user choice name here), Enter Activation Code:")
+
+if code == "":
+    First_Time()
+
+elif code == passcode:
+    Normal()
+
+else:
+    code = input("wrong")
+
+    
